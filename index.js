64007d4ffb7ee24932c411bb0de5156a45c6c7cc
Apurbaroy01 <apurbaroy00011@gmail.com>
2025-08-19 08:49:11 +0600

    16th

--- index.js
+++ index.js
@@ -56,11 +56,11 @@ async function run() {
 
     app.post('/jwt', async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.secret_key, { expiresIn: '1h' });
+      const token = jwt.sign(user, process.env.secret_key, { expiresIn: '5h' });
       res.cookie('token', token, {
         httpOnly: true,
-        secure: false, // Set to true if using HTTPS
-        sameSite: "lax", // Adjust as needed
+        secure: process.env.NODE_ENV==='production' ,
+        sameSite: process.env.NODE_ENV==='production' ? "none": "strict"
       }).send({ success: true, token });
     });
 
@@ -68,7 +68,8 @@ async function run() {
       res
       .clearCookie('token',{
         httpOnly:true,
-        secure: false
+        secure: process.env.NODE_ENV==='production' ,
+        sameSite: process.env.NODE_ENV==='production' ? "none": "strict"
       })
       .send({})
     })
